f80fb6eddf4a010bcda1aa7792775e4980745b72
adam <maohaijian@xiimoon.com>
2026-03-30 13:42:00 +0800

    Add boss exclude support for NameplateCastMarker

--- Raids/Aberrus/AssaultOfTheZaqali.lua
+++ Raids/Aberrus/AssaultOfTheZaqali.lua
@@ -10,7 +10,9 @@ function Boss:OnMythicEncounterStart()
     self.isActive = true
     local marker = addon.modules and addon.modules["Common.NameplateCastMarker"]
     if marker and type(marker.Enable) == "function" then
-        marker:Enable()
+        marker:Enable({
+            exclude = {"boss1"},
+        })
     end
 end
 
